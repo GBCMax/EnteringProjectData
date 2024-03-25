887851068f613dc966be7357ca6bc7311141b667
EliseevMV <maks_on_max@mail.ru>
2024-03-25 20:24:46 +0700

    Добавлена страница изменения сотрудника. Необходимо реализовать добавление.

--- EnteringProjectData/Controllers/HomeController.cs
+++ EnteringProjectData/Controllers/HomeController.cs
@@ -22,7 +22,6 @@ namespace EnteringProjectData.Controllers
             _dbContext = dBContext;
             _projectRepository = new ProjectRepository(dBContext);
             _employeeRepository = new EmployeeRepository(dBContext);
-            //FillDB();
         }
 
         public IActionResult Main(int? projectID)
@@ -39,20 +38,42 @@ namespace EnteringProjectData.Controllers
             return View(_projectRepository);
         }
 
+        //public IActionResult AddEmployeeInProject(int projectID)
+        //{
+        //    Project? project = _dbContext.Project.FirstOrDefault(p => p.ProjectId == projectID);
+        //    if (project != null)
+        //    {
+        //        return PartialView(_employeeRepository);
+        //    }
+        //    return BadRequest();
+        //}
+
+        //[HttpPost]
+        //public IActionResult AddEmployeeInProject(EmployeeRepository _employeeRepository)
+        //{
+
+        //}
+
         public IActionResult ChangeProject(Project project)
         {
             return View(project);
         }
 
-        //[HttpPut]
-        //public IActionResult ChangeProject(int projectID)
-        //{
-        //    Project? project = _dbContext.Project.FirstOrDefault(p => p.ProjectId == projectID);
-        //    return View(project);
-        //}
-
-        public IActionResult Employees()
+        public IActionResult ChangeEmployee(Employee employee)
         {
+            return View(employee);
+        }
+
+        public IActionResult Employees(int? employeeID)
+        {
+            if (employeeID != null)
+            {
+                Employee? employee = _dbContext.Employee.FirstOrDefault(emp => emp.EmployeeID == employeeID);
+                if (employee != null)
+                {
+                    return RedirectToAction("ChangeEmployee", employee);
+                }
+            }
             return View(_employeeRepository);
         }
         public IActionResult CreateProject()
@@ -63,7 +84,6 @@ namespace EnteringProjectData.Controllers
         [HttpPost]
         public IActionResult CreateProject([FromForm] Project project)
         {
-            //project.Employees = new List<Employee>();
             if (ModelState.IsValid)
             {
                 _projectRepository.AddProject(project);
@@ -80,7 +100,6 @@ namespace EnteringProjectData.Controllers
         [HttpPost]
         public IActionResult CreateEmployee([FromForm] Employee employee)
         {
-            //employee.Projects = new List<Project>();
             if(ModelState.IsValid)
             {
                 _employeeRepository.AddEmployee(employee);
@@ -93,54 +112,5 @@ namespace EnteringProjectData.Controllers
 		{
 			return View();
 		}
-
-        //private void FillDB()
-        //{
-        //    Project project1 = new Project
-        //    {
-        //        ProjectName = "Project1",
-        //        CustomerCompanyName = "Customer1",
-        //        ExecutingCompanyName = "Executor1",
-        //        StartDate = DateTime.Now.Date,
-        //        EndDate = DateTime.Now.Date.AddDays(31),
-        //        Priority = 1
-        //    };
-        //    Project project2 = new Project
-        //    {
-        //        ProjectName = "Project2",
-        //        CustomerCompanyName = "Customer2",
-        //        ExecutingCompanyName = "Executor2",
-        //        StartDate = DateTime.Now.Date.AddDays(-31),
-        //        EndDate = DateTime.Now.Date,
-        //        Priority = 2
-        //    };
-        //    if (!_indexViewModel.Projects.Contains(project1) && !_indexViewModel.Projects.Contains(project2))
-        //    {
-        //        _indexViewModel.Projects.Add(project1);
-        //        _indexViewModel.Projects.Add(project2);
-        //    }
-        //    Employee employee1 = new Employee(1)
-        //    {
-        //        FirstName = "Максим",
-        //        SecondName = "Елисеев",
-        //        Patronymic = "Викторович",
-        //        Email = "maks_on_max@mail.ru"
-        //    };
-        //    Employee employee2 = new Employee(2)
-        //    {
-        //        FirstName = "Алексей",
-        //        SecondName = "Зайцев",
-        //        Patronymic = "Лох",
-        //        Email = "почта алексея зайцева"
-        //    };
-        //    if (!_indexViewModel.Employees.Contains(employee1) && !_indexViewModel.Employees.Contains(employee2))
-        //    {
-        //        _indexViewModel.Employees.Add(employee1);
-        //        _indexViewModel.Employees.Add(employee2);
-        //        project1.AddEmployee(employee1);
-        //        project1.AddEmployee(employee2);
-        //        project2.AddEmployee(employee1);
-        //    }
-        //}
 	}
 }
